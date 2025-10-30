8eb3942f26abf4d3f3f40dffbc6bacbdb80445d4
Daniel Victor de Oliveira Rodrigues <oliveira0196@gmail.com>
2025-10-30 11:50:02 -0300

    main

--- app/page.tsx
+++ app/page.tsx
@@ -2,29 +2,27 @@ import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
-import {events} from "@/lib/constants";
-import type { EventItem } from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
     'use cache';
     cacheLife('hours')
-    // const response = await fetch(`${BASE_URL}/api/events`);
-    // const { events } = await response.json();
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
 
     return (
         <section>
-            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Cant Miss</h1>
             <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
 
             <ExploreBtn />
 
             <div className="mt-20 space-y-7">
                 <h3>Featured Events</h3>
-                <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
-                    {events && events.length > 0 && events.map((event: EventItem) => (
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event: IEvent) => (
                         <li key={event.title} className="list-none">
                             <EventCard {...event} />
                         </li>
