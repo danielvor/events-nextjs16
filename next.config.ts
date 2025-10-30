90985e328516ff9a193fc12f7c463a252df4fae9
Daniel Victor de Oliveira Rodrigues <oliveira0196@gmail.com>
2025-10-30 13:22:52 -0300

    main

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone', // ou 'export' se for estático
+  trailingSlash: true,
     typescript: {
         ignoreBuildErrors: true,
     },
     cacheComponents: true,
     images: {
+        unoptimized: true, // se estiver usando Vercel
         remotePatterns: [
             {
                 protocol: 'https',
